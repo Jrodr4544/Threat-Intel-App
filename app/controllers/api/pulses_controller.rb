a4f19f487e62987f4d21a34576a594dabeb7bb2b
Jelson Rodriguez <maskdbandit@gmail.com>
2022-10-11 18:46:26 -0400

    added rest-client request to OTX, and rendering the results as json

--- app/controllers/api/pulses_controller.rb
+++ app/controllers/api/pulses_controller.rb
@@ -1,7 +1,12 @@
+require 'rest-client'
+
 class Api::PulsesController < ApplicationController
-  
+  URL = "https://otx.alienvault.com/api/v1/pulses/subscribed?page=1" 
+
   def index
-    render html: '<div>HELLO, THIS IS THE INDEX PAGE</div>'.html_safe
+    #byebug
+    req = RestClient.get(URL, headers={"X-OTX-API-KEY": ENV['OTX_API_KEY']}) 
+    render :json => req
   end
 
 end
