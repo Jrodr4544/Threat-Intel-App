5303b285b01a9b08fbc24652fd2833c86a6e9725
Jelson Rodriguez <maskdbandit@gmail.com>
2022-10-13 20:16:56 -0400

    removed name column in order to add a uniq name

--- db/migrate/20221011225113_add_attributes_to_pulses.rb
+++ db/migrate/20221011225113_add_attributes_to_pulses.rb
@@ -1,6 +1,5 @@
 class AddAttributesToPulses < ActiveRecord::Migration[5.1]
   def change
-    add_column :pulses, :name, :string
     add_column :pulses, :description, :text
     add_column :pulses, :author_name, :string
     add_column :pulses, :tlp, :string
