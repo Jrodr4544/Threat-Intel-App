7151c4c7e4ad1063ccbbb7a1c8584b8103a316fe
Jelson Rodriguez <maskdbandit@gmail.com>
2022-10-19 18:54:14 -0400

    added pulse resources

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   namespace :api do
-    root to: "pulses#index"
+    resources :pulses do
+      #root to: "pulses#index"
+    end
   end
 end
