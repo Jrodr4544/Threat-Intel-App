3975386e5bb0bc89142b98c573f29f7fb6b04807
Jelson Rodriguez <maskdbandit@gmail.com>
2022-10-19 18:18:12 -0400

    added Link component from react-router-dom in order to render components rather than load pages

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -1,42 +1,22 @@
-import React from 'react';
-import { Navbar, Nav } from 'react-bootstrap';
-import { NavLink } from 'react-router-dom';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import {Link} from 'react-router-dom';
 
-const Navigation = () => {
-	return (
-		<Navbar inverse collapseOnSelect>
-		        <Navbar.Header>
-		          <Navbar.Brand>
-		            <NavLink
-		              to='/'
-		            >
-				Threat Intel App			
-		            </NavLink>
-		          </Navbar.Brand>
-		          <Navbar.Toggle />
-		        </Navbar.Header>
-		        <Navbar.Collapse>
-		          <Nav>
-		              <Navbar.Brand>
-		                <NavLink
-		                  to="/pulses"
-		                >
-				  Pulses
-		                </NavLink>
-		              </Navbar.Brand>
-		          </Nav>
-		          <Nav pullRight>
-		            <Navbar.Brand>
-		                <NavLink
-		                  to="/about"
-		                >
-		                  About
-		                </NavLink>
-		            </Navbar.Brand>
-		          </Nav>
-		        </Navbar.Collapse>
-		      </Navbar>
-	)
+function Navigation() {
+	  return (
+			<Navbar bg="dark" variant="dark">
+		  	    <Navbar.Brand>
+		  		<Nav.Link as={Link} to='/'>
+		  			Threat-Intel-App
+		  		</Nav.Link>
+		  	    </Navbar.Brand>
+		            <Nav className="me-auto">
+		              <Nav.Link as={Link} to="/pulses">Pulses</Nav.Link>
+		              <Nav.Link as={Link} to="/about">About</Nav.Link>
+		            </Nav>
+		        </Navbar>
+		    );
 }
 
 export default Navigation;
