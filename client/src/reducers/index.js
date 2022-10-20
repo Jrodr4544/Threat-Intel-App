9b530d0e75527f1deea0404aa6a640b4c800c8ff
Jelson Rodriguez <maskdbandit@gmail.com>
2022-10-20 17:35:07 -0400

    added filtersReducer to rootReducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
 import pulsesReducer from './pulsesReducer';
+import filtersReducer from './filtersReducer';
 // Need to create and add filter reducer
 
 export default combineReducers({
-    pulses: pulsesReducer
+    pulses: pulsesReducer,
+    filters: filtersReducer
 })
