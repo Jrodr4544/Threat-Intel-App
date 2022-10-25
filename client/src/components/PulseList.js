ac44ed836f29e83ac036c7614aab578c05b8122e
Jelson Rodriguez <maskdbandit@gmail.com>
2022-10-25 18:47:46 -0400

    added list element to each card link and its reference. also created Text Cards for each indicator and added flex-container css class to them

--- client/src/components/PulseList.js
+++ client/src/components/PulseList.js
@@ -17,21 +17,40 @@ const PulseList = ({pulses}) => {
 			<Card.Subtitle className="mb-2 text-muted">TLP: { pulse.tlp }</Card.Subtitle>
 			<Card.Subtitle className="mb-2 text-muted">Adversary: { (pulse.adversary === "") ? "None" : pulse.adversary }</Card.Subtitle>
 			{/* Need to add indicators, industries, malware_families, references, and tags */}
-			<Card.Title>References:</Card.Title>
 
+			<div class="flex-container">Indicators:
+			{
+				(pulse.indicators.length > 0) ?
+					pulse.indicators.map( indicator => {
+						return (
+							<div>
+								<Card.Text className="indicators" href="#" key={indicator.id}>{indicator.category}: {indicator.value}</Card.Text>
+							</div>
+						)
+					})
+				:
+					null
+			}	
+			</div>
+
+			{/* Pulse References */}
+
+
+			<ul>{(pulse.references.length > 0) ? "References:" : null}
 			{
 				(pulse.references.length > 0) ?
 					pulse.references.map( reference => {
 						return (
-								<Card.Link href="#" key={reference.id}>{reference.resource}</Card.Link>
+								<li><Card.Link href="#" key={reference.id}>{reference.resource}</Card.Link></li>
 						)
 					})
 				:
-						<p>None</p>
+					null
 			}	
+			</ul>
 
-			<Card.Link href="#">Card Link</Card.Link>
 			<Card.Link href="#">Another Link</Card.Link>
+			<Card.Link href="#">Card Link</Card.Link>
 		      </Card.Body>
 		    </Card>
 			<br/>
