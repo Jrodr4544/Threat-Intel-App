82e881be203078848914e00bbab558e90d9e2d8a
Jelson Rodriguez <maskdbandit@gmail.com>
2022-10-27 19:35:38 -0400

    removed unnecessary code

--- client/src/reducers/filtersReducer.js
+++ client/src/reducers/filtersReducer.js
@@ -1,25 +1,12 @@
-export default (state = {loading: false, filters: []}, action) => {
+export default (state = [], action) => {
 
     switch (action.type) {
-        case "LOADING_FILTERS":
+        case "SET_FILTERS":
         debugger
-		console.log('LOADING_FILTERS')
-            return { ...state, loading: true }
-
-        case "FETCH_FILTERS":
-        debugger
-        console.log('FETCHING_FILTERS')
-        console.log(action)
-            return { ...state, filters: action.filters }
-
-        case "FETCH_ERROR_GENERATED":
-        debugger
-        console.log('FETCH_ERROR_GENERATED')
-        console.log(action)
-            return { ...state, loading: action } 
-    
+        console.log('SETTING FILTERS')
+            return action.filters
+        
         default:
             return state
     }
-
 }
