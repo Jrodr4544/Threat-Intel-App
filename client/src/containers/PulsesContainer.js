330ebcb755a34ed63f52ce4f73da32fd8ad0b3ef
Jelson Rodriguez <maskdbandit@gmail.com>
2022-10-27 19:34:56 -0400

    added and binded handleFilterPulses function to FilterContainer which controls filtering based on input. Passed handleFilterPUlses as props to FilterContainer and also passed filteredPulses as props to PulseList

--- client/src/containers/PulsesContainer.js
+++ client/src/containers/PulsesContainer.js
@@ -9,21 +9,44 @@ class PulsesContainer extends Component {
 	constructor(props) {
 		super(props);
 
+    		this.handleFilterPulses = this.handleFilterPulses.bind(this);
 		this.state = {
-			pulses: []
+			pulses: [],
+		filteredPulses: []
 		}
 	
 	}
 
 	componentDidMount() {
-		debugger
+		//debugger
 		this.props.fetchPulses().then(
 			response => {
 				this.setState({
-					pulses: response 
+					pulses: response,
+					filteredPulses: response
 				})
 			}
 		)
+	}
+
+
+	handleFilterPulses(filter) {
+	    //debugger
+	    // Filter based on tags, and input 
+	    //const filterPulses = this.state.pulses.filter(pulse =>
+	     // (pulse.tags.filter(tag => tag.includes(filter.tag)).length > 1) &&
+	      //(pulse.x_mitre_platforms.filter(platform => platform.includes(filter.platformFilter)).length > 0)
+	    //);
+
+	    //const result = filterPulses.filter(pulse => Object.values(pulse).flat().join('').includes(filter['inputFilter']));
+
+	    const result = this.state.pulses.filter(pulse => Object.values(pulse).flat().join('').includes(filter['inputFilter']));
+		
+	    return this.setState({
+	      ...this.state,
+	      filteredPulses: result
+	    })
+
 	}
 
 	render() {
@@ -31,9 +54,13 @@ class PulsesContainer extends Component {
 		return (
 			<div>
 				<h1>PulsesContainer</h1>
-				<FilterContainer />
+
+				<FilterContainer 
+					filterPulses={ this.handleFilterPulses }
+				/>
+
 				<PulseList 
-					pulses={ this.state.pulses }
+					pulses={ this.state.filteredPulses }
 				/>
 			</div>
 		)
