a359621198cf17a8101719ad949c033bac16eb4c
Jelson Rodriguez <maskdbandit@gmail.com>
2022-11-02 21:26:56 -0400

    added dummy links to right side of navbar

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -1,11 +1,13 @@
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import Container from 'react-bootstrap/Container';
 import {Link} from 'react-router-dom';
 
 function Navigation() {
 	  return (
-			<Navbar bg="dark" variant="dark">
+		  {/* find template example in react-bootstrap with collapsable navbar and implement here */}
+			<Navbar expand="lg" bg="dark" variant="dark">
+		  		<Container>
 		  	    <Navbar.Brand>
 		  		<Nav.Link as={Link} to='/'>
 		  			Threat-Intel-App
@@ -15,6 +17,14 @@ function Navigation() {
 		              <Nav.Link as={Link} to="/pulses">Pulses</Nav.Link>
 		              <Nav.Link as={Link} to="/about">About</Nav.Link>
 		            </Nav>
+
+			    <Nav>
+		              <Nav.Link href="#deets">More deets</Nav.Link>
+		              <Nav.Link href="#memes">
+		                Dank memes
+		              </Nav.Link>
+		            </Nav>
+		  		</Container>
 		        </Navbar>
 		    );
 }
