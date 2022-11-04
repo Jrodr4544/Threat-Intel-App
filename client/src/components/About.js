2802bc17ce74169db44fa26af3554e31f0212662
Jelson Rodriguez <maskdbandit@gmail.com>
2022-11-03 20:22:26 -0400

    added app summary to about page within a Card bootstrap component

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -1,9 +1,16 @@
-
+import Card from 'react-bootstrap/Card';
 
 const About = () => {
 
 	return (
-		<h1> About </h1>
+
+		    <Card>
+		      <Card.Body>
+			<h1> About </h1>
+			Threat-Intel-App is a web application that interacts with AlienVault's Open Threat Exchange(OTX). It queries the OTX DirectConnect API for threat intelligence data that is returned in the form of "pulses". Each pulse has its own Indicators of Compromise (IOCs), and includes IP addresses, domains, hostnames, file hashes (MD5, SHA1, SHA256, PEHASH, etc), CVE numbers, URLs, email addresses, file paths, mutex names, and more. 
+		      </Card.Body>
+		    </Card>
+
 	)
 }
 
