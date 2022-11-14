5303656ddac8854997d5d08c3bacf1fa19b0b910
Jelson Rodriguez <maskdbandit@gmail.com>
2022-11-14 13:24:31 -0500

    removed unique index on name

--- db/migrate/20221014000455_add_name_uniqueness_to_pulse.rb
+++ db/migrate/20221014000455_add_name_uniqueness_to_pulse.rb
@@ -1,6 +1,6 @@
 class AddNameUniquenessToPulse < ActiveRecord::Migration[5.1]
   def change
     add_column :pulses, :name, :string
-    add_index :pulses, :name, unique: true
+#    add_index :pulses, :name, unique: true
   end
 end
