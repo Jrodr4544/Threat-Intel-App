175dfde1946436c74e6a0c4c84467d1a727cc248
Jelson Rodriguez <maskdbandit@gmail.com>
2022-11-16 19:07:02 -0500

    added route that loads UserProfileContainer

--- client/src/App.js
+++ client/src/App.js
@@ -11,16 +11,18 @@ import PulsesContainer from './containers/PulsesContainer';
 import Navigation from './components/Navigation';
 import About from './components/About';
 import Index from './components/Index';
+import UserProfileContainer from './containers/UserProfileContainer';
 
 function App() {
   return (
     <div className="App">
-	
+
 	  <Router>
 	  <Navigation />
 	  	<Routes>
 	  		<Route path='/pulses' element={<PulsesContainer />} />
 	  		<Route path='/about' element={<About />} />
+			<Route path='/login' element={<UserProfileContainer/>} />
 	  		<Route exact path='/' element={<Index />} />
 	  {/* <Route exact path='/' render={() => (
 				<div className='App'><h1>Welcome</h1></div>
