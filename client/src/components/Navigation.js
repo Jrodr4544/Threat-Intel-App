7d636c875ad1da0d2df53e8099f1831fb51966c7
Jelson Rodriguez <maskdbandit@gmail.com>
2022-11-16 19:08:30 -0500

    added link to login

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -13,15 +13,15 @@ function Navigation() {
 		  			Threat-Intel-App
 		  		</Nav.Link>
 		        </Navbar.Brand>
-		      	<Navbar.Toggle aria-controls="basic-navbar-nav"/> 
+		      	<Navbar.Toggle aria-controls="basic-navbar-nav"/>
 			<Navbar.Collapse id="basic-navbar-nav">
 			  <Nav className="me-auto">
 		              <Nav.Link as={Link} to="/pulses">Pulses</Nav.Link>
 		              <Nav.Link as={Link} to="/about">About</Nav.Link>
 			  </Nav>
-			  
+
 			  <Nav>
-			    <Nav.Link href="#test">Test</Nav.Link>
+			    <Nav.Link as={Link} to="/login">Login</Nav.Link>
 			  </Nav>
 			</Navbar.Collapse>
 		  </Container>
@@ -30,4 +30,3 @@ function Navigation() {
 }
 
 export default Navigation;
-
